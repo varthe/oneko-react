2fac2b8ae5ad1340dcc31aa342d7f62ce57c48ee
varthe <gitenjoyer@plvar.com>
2026-04-05 16:44:22 +0200

    npm init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "index.ts",
+      name: "Oneko",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+})
